bffc54cb1045aa1f6f30b23301bd6b5c6bd06b36
ashuxldr <ashuxldr@gmail.com>
2022-02-07 17:41:29 +0530

    insert Node at Head

---
+++ LinkedList/insertNodeAtHead.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class LinkedList
+{
+    Node *head;
+
+public:
+    LinkedList()
+    {
+        head = NULL;
+    }
+    void insertNodeAtTail(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            return;
+        }
+        Node *curr = head;
+        while (curr->next != NULL)
+        {
+            curr = curr->next;
+        }
+        curr->next = newNode;
+    }
+    void insertNodeAtHead(int val)
+    {
+        Node *newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+    }
+    void display()
+    {
+        Node *curr = head;
+        if (head == NULL)
+        {
+            cout << "List empty" << endl;
+            return;
+        }
+        while (curr != NULL)
+        {
+            cout << curr->data << "-->";
+            curr = curr->next;
+        }
+    }
+};
+
+int main()
+{
+    LinkedList ll;
+    ll.insertNodeAtTail(1);
+    ll.insertNodeAtTail(2);
+    ll.insertNodeAtTail(3);
+    ll.insertNodeAtTail(4);
+    ll.insertNodeAtTail(5);
+    ll.insertNodeAtTail(6);
+    ll.insertNodeAtTail(7);
+    ll.insertNodeAtTail(8);
+    ll.insertNodeAtHead(9);
+    ll.insertNodeAtHead(10);
+    ll.insertNodeAtHead(11);
+    ll.display();
+    return 0;
+}
