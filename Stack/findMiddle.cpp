4b8ef80e25797bdd63197fd7596a5af93a308ae8
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-03-22 19:27:20 +0530

    find middle

---
+++ Stack/findMiddle.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int x){
+        this->data = x;
+    }
+};
+
+class Stack{
+    Node *head = NULL;
+    Node *mid = NULL;
+    int size = 0;
+public:
+    void push(int data)
+    {
+        Node *temp = new Node(data);
+        if (size==0)
+        {
+            head = temp;
+            mid = temp;
+            size++;
+            return;
+        }
+ 
+        head->next = temp;
+        temp->prev = head;
+        head = head->next;
+        if (size%2==1)
+        {
+            mid = mid->next;
+        }
+        size++;
+    }
+    void pop()
+    {
+        if (size!=0)
+        {
+            if (size==1)
+            {
+                head = NULL;
+                mid = NULL;
+            }
+            else
+            {
+                head = head->prev;
+                head->next = NULL;
+                if (size%2==0)
+                {
+                    mid = mid->prev;
+                }
+            }
+            size--;
+        }
+    }
+ 
+    int findMiddle()
+    {
+        if (size==0)
+        {
+            return -1;
+        }
+        return mid->data;
+    }
+    void deleteMiddle()
+    {
+        if (size!=0)
+        {
+            if (size==1)
+            {
+                head = NULL;
+                mid = NULL;
+            }
+            else if (size==2)
+            {
+                head = head->prev;
+                mid = mid->prev;
+                head->next =NULL;
+            }
+            else
+            {
+                mid->next->prev = mid->prev;
+                mid->prev->next = mid->next;
+                if (size%2==0)
+                {
+                    mid = mid->prev;
+                }
+                else
+                {
+                    mid = mid->next;
+                }
+            }
+            size--;
+        }
+    }
+};
+int main()
+{
+    Stack st;
+    st.push(11);
+    st.push(22);
+    st.push(33);
+    st.push(44);
+    st.push(55);
+    st.pop();
+    st.pop();
+    st.pop();
+    cout<<st.findMiddle()<<endl;
+    st.deleteMiddle();
+    cout<<st.findMiddle()<<endl;
+    return 0;
+}
