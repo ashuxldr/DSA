b728a2c7f85ee4aa1de54eea6fafd62f6c5b9a3f
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-07-08 23:09:36 +0530

    recursion start

---
+++ Recursion/power.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int &x, int n){
+    if(n==0)
+    return 1;
+
+    return (x*power(x, n-1));
+}
+
+
+int main(){
+    int x = 2;
+    cout<<power(x, 6);
+    return 0;
+}
